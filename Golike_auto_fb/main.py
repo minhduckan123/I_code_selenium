f75c9644c9f6b5bbeb84e55a0b38faebd12a32db
ducdut <93233277+minhduckan123@users.noreply.github.com>
2022-03-10 22:26:21 +0700

    First

--- Golike_auto_fb/main.py
+++ Golike_auto_fb/main.py
@@ -1,87 +1,86 @@
-import undetected_chromedriver as uc
+from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import time
-import ssl
 
 
-# remove verify SSL
-ssl._create_default_https_context = ssl._create_unverified_context
-
-driver = uc.Chrome("/usr/lib/chromium-browser/chromedriver")
+a = int(input("Nhập số lượng chạy: "))
+       
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--user-data-dir=C:/Users/Lenovo T460/AppData/Local/Google/Chrome/User Data")
+chrome_options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 driver.maximize_window()
 
-i_do = int(input())
-# i_do = 10
-f = open("account.txt", "r")
-i = f.readlines()
+driver.get("https://app.golike.net/jobs")
+kiem_tien_ngay = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/span/div[2]/div/div/div[1]/div[2]/div/div")))
+kiem_tien_ngay.click()
+print("\nkiem tien ngay\n")
 
-username_f, password_f = i[0].strip().split()
-username, password = i[1].strip().split()
-driver.get("https://www.facebook.com/")
-time.sleep(5)
-driver.find_element(By.XPATH, "//*[@id=\"email\"]").send_keys(username_f)
-driver.find_element(By.XPATH, "//*[@id=\"pass\"]").send_keys(password_f, Keys.RETURN)
+def runtest():
+       chon_job = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/div/div[2]/div[2]/span/div[1]/div/div/div/div")))
+       try:chon_job.click()
+       except:
+              try:
+                     chon_job = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/div/div[2]/div[2]/span/div/div/div/div/div/div[2]/div[1]/div[1]/span")))
+                     chon_job.click()
+              except:driver.execute_script('document.querySelector("#app > div > div:nth-child(1) > div.page-container > div > div:nth-child(2) > div:nth-child(3) > span > div > div").click()')
+       print("\nchon job\n")
 
-time.sleep(5)
-driver.get('https://app.golike.net/home')
+       text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/div[1]/div/div/div/div/div[2]/div[1]/div/span")))
+       check_text = text.text
+       print("\nlay van ban\n")
 
-driver.find_element(By.XPATH, "//input[@type = 'text']").send_keys(username)
-driver.find_element(By.XPATH, "//input[@type = 'password']").send_keys(password)
-driver.find_element(By.XPATH, "//button[@type = 'submit']").click()
+       sleep(7)
+       driver.execute_script('document.querySelector("#app > div > div:nth-child(1) > div.page-container > div:nth-child(2) > div:nth-child(2) > div > div > a:nth-child(3) > div.col.px-0 > h6.font-bold.font-18").click()')
+       driver.switch_to.window(driver.window_handles[1])
+       print("\nlam job\n")
 
-time.sleep(10)
-element_check = WebDriverWait(driver, 1800).until(EC.presence_of_element_located((By.XPATH, "//button[@type = 'submit']")))
-element_check.click()
-time.sleep(5)
-driver.find_element(By.XPATH, "//*[@id='app']/div/div[2]/div/div/div[2]").click()
-time.sleep(5)
-driver.find_element(By.XPATH, "//*[@id='app']/div/div[1]/div[2]/span/div[2]/div/div/div[1]/div[2]/div/div").click()
-time.sleep(10)
+       if "THEO" in check_text:
+              
+              try:
+                     follow = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Follow']")))
+                     follow.click()
+              except:
+                     ba_cham = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@aria-expanded="false"]')))
+                     ba_cham.click()
+                     follow = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@role="menuitem"]')))
+                     follow.click()
+                     sleep(2)
+              print("\nFollow\n")
+       elif "LIKE" in check_text:
+              like = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Like']")))
+              like.click()
+              print("\nLike\n")
+       driver.close()
+       driver.switch_to.window(driver.window_handles[0])
+       sleep(7)
+       driver.execute_script('document.querySelector("#app > div > div:nth-child(1) > div.page-container > div:nth-child(2) > div.card.card-job-detail.hand > div > div").click()')
+       print("\nhoan thanh\n")
+       sleep(7)
+       driver.execute_script('document.querySelector("body > div.swal2-container.swal2-center.swal2-fade.swal2-shown > div > div.swal2-actions > button.swal2-confirm.swal2-styled").click()')
+       print("\nxac nhan hoan thanh\n")
 
-while i_do > 0:
-    i_do -=1
-    try:
-        element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/div/div[2]/div[2]/span/div/div")))
-        element.click()
-    except:
-        try:
-            element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/div/div[2]/div[2]/span/div/div/div/div/div/div[2]/div[1]/div[2]/span/span")))
-            element.click()
-        except:
-            pass
-        
-    try:
-        time.sleep(5)
-        driver.find_element(By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[2]/div[1]/div/div/a[2]").click()
-        time.sleep(15)
-        try:
-            driver.switch_to.window(driver.window_handles[1])
-            element_likes = driver.find_elements(By.XPATH, "//div[@aria-label='Like']")
-            for element in element_follows:
-                try:
-                    element.click()
-                    time.sleep(4)
-                except:
-                    pass
-            element_follows = driver.find_elements(By.XPATH, "//div[@aria-label='Follow']")
-            for element in element_likes:
-                try:
-                    if element == element_likes[2]:
-                        break
-                    element.click()
-                    time.sleep(4)
-                except:
-                    pass
-            driver.close()
-        except:
-            pass
-        driver.switch_to.window(driver.window_handles[0])
-        driver.find_element(By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[2]/div[2]").click()
-        time.sleep(5)
-        driver.find_element(By.XPATH, "//button[@type = 'button'][@class='swal2-confirm swal2-styled']").click()
-        print(i_do, "success!")
-    except:
-        print("error{}!".format(i_do))
+       
+for i in range(a):
+       try:
+              runtest()
+              print(f"Hoan thanh job {i+1}")
+       except:
+              print(f"Loi job {i+1}")
+              try:
+                     sleep(20)
+                     driver.execute_script('document.querySelector("body > div.swal2-container.swal2-center.swal2-fade.swal2-shown > div > div.swal2-actions > button.swal2-confirm.swal2-styled").click()')
+              except:
+                     pass
+              driver.switch_to.window(driver.window_handles[0])
+              driver.get("https://app.golike.net/jobs")
+              kiem_tien_ngay = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[2]/span/div[2]/div/div/div[1]/div[2]/div/div")))
+              kiem_tien_ngay.click()
+              print("\nkiem tien ngay\n")
+              
+
+
+#  send_email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/footer/section[1]/section[1]/section[1]/form/input[1]")))
